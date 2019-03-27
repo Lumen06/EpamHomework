0c3846ca11ba72e42278c2a793d9ba209cf7fe6d
Timur Kustov <46898159+Lumen06@users.noreply.github.com>
2019-03-27 10:51:42 +0300

    Added Sax Parser

--- src/homework/Storage/initor/datasourcereader/sax/CountriesAndCitiesSaxParser.java
+++ src/homework/Storage/initor/datasourcereader/sax/CountriesAndCitiesSaxParser.java
@@ -1,11 +1,13 @@
 package homework.Storage.initor.datasourcereader.sax;
 
 import homework.Country.domain.BaseCountry.Country;
+import homework.Storage.initor.datasourcereader.CountriesAndCitiesTxtParser;
 
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class CountriesAndCitiesSaxParser implements XmlParser<List<Country>> {
@@ -19,7 +21,17 @@ public class CountriesAndCitiesSaxParser implements XmlParser<List<Country>> {
         CountriesAndCitiesSaxHandler handler = new CountriesAndCitiesSaxHandler();
         saxParser.parse(new File(file), handler);
 
-        return handler.getPersons();
+        return handler.getCountriesList();
+    }
+
+    public static void main(String[] args) throws Exception{
+        CountriesAndCitiesSaxParser countriesAndCitiesSaxParser = new CountriesAndCitiesSaxParser();
+        List <Country> list = countriesAndCitiesSaxParser.parse("C:\\Users\\Royal\\IdeaProjects\\TravelAgency\\resources\\dataInit.xml");
+
+        for (Country country : list) {
+            System.out.println(country.toString());
+
+        }
     }
 }
-}
+
